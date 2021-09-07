d49c07ad46ac4aa3060184fb6afa1f4cf80a6087
erisazahra25 <erisazahra25@gmail.com>
2021-09-07 13:45:44 +0700

    membuat menjadi landing page

--- resources/views/Menu_Utama/Beranda.blade.php
+++ resources/views/Menu_Utama/Beranda.blade.php
@@ -93,4 +93,165 @@ header.classList.toggler ("sticky", window.scrollY > 0);
   <script>
     AOS.init();
   </script>
+
+<section id="koleksi">
+         
+            <div class="gallery mt-5">
+              <br>
+              <div class="text-center">KOLEKSI</div>
+              <br>
+              <br>
+              <div class="card">
+                  <div class="card-image1"></div>
+                  <div class="card-text">
+                      <!-- <div class="jenis">Prasejarah</div> -->
+                      <p>AUSTRONESIA, masyarakat yang memperkenalkan cara bercocok tanam hingga domestikasi binatang di kepulauan Indonesia. Penutur Austronesia muncul sekitar 7000 - 6000 SM yang lalu di Taiwan Kemudian menyebar keberbagai belahan dunia sejak 5000 SM.
+                      </p>
+                  </div>
+                  <div class="card-stats1">
+                      <div class="stat mt-3">
+                          <div class="type">Prasejarah</div>
+                      </div>
+                  </div> 
+              </div>
+              <div class="card">
+                  <div class="card-image2"></div>
+                  <div class="card-text">
+                      <!-- <div class="jenis">Hindu-Budha</div> -->
+                      <p>
+                          PADUKA BHATTARA, Penguasa pertama ujung Timur Jawa (Blambangan) yaitu Sri Nagarawardhani. Berselang beberapa tahun Sri Nagarwardhani pindah tugas sehingga pemerintahan Blambangan diserahkan suaminya yang juga bergelar Paduka Bhattara Wirabumi.
+                      </p>
+                  </div>
+                  <div class="card-stats2">
+                      <div class="stat mt-3">
+                          <div class="type">Hindu-Budha</div>
+                      </div>
+                  </div> 
+              </div>
+              <div class="card">
+                  <div class="card-image3"></div>
+                  <div class="card-text">
+                      <!-- <div class="jenis">Islam</div> -->
+                      <p>This hobby is my newest hobby, I'd love this activities around a year ago. For me it's very important
+                          as a college student to develop literacy. For now I really like book of self improvement. I'd like this
+                           genre because good for my life.
+                      </p>
+                  </div>
+                  <div class="card-stats3">
+                      <div class="stat mt-3">
+                          <div class="type">Islam</div>
+                      </div>
+                  </div> 
+              </div>
+
+
+
+              <div class="card">
+                  <div class="card-image4"></div>
+                  <div class="card-text">
+                      <!-- <div class="jenis">Kolonial</div> -->
+                      <p>This hobby is my newest hobby, I'd love this activities around a year ago. For me it's very important
+                          as a college student to develop literacy. For now I really like book of self improvement. I'd like this
+                           genre because good for my life.
+                      </p>
+                  </div>
+                  <div class="card-stats4">
+                      <div class="stat mt-3">
+                          <div class="type">Kolonial</div>
+                      </div>
+                  </div> 
+              </div>
+          </div>
+</section>
+
+<section id="AboutUs">
+
+    <div id="lambang" class="row">
+      <div class="col text-center">
+        <h2>
+          Lambang Museum Blambangan
+        </h2>
+      </div>
+    </div>
+    <div id="gambar" class="row text-center mt-5">
+      <div class="col">
+        <img src="https://i.ibb.co/0DGD8Kr/download.jpg" alt="" height="250px" width="250px">
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row text-center mt-5">
+        <div class="col">
+          <h3>
+            Makna Lambang Museum Blambangan
+          </h3>
+        </div>
+      </div>
+      <div class="row justify-content-center mt-5">
+        <div class="col">
+          <h4 class="text-center">Makna Bentuk Lambang Museum Blambangan</h4>
+          <ol>
+            <li>Makna Lambang Museum Blambangan</li>
+            <dd> Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit aliquam porro qui ratione at, fuga velit vero omnis est mollitia repellendus aperiam ab a corrupti harum, odio quae neque maiores?</dd>
+            <li>Makna Lambang Museum Blambangan</li>
+            <dd> Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit aliquam porro qui ratione at, fuga velit vero omnis est mollitia repellendus aperiam ab a corrupti harum, odio quae neque maiores?</dd>
+            <li>Makna Lambang Museum Blambangan</li>
+            <dd> Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit aliquam porro qui ratione at, fuga velit vero omnis est mollitia repellendus aperiam ab a corrupti harum, odio quae neque maiores?</dd>
+            <li>Makna Lambang Museum Blambangan</li>
+            <dd> Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit aliquam porro qui ratione at, fuga velit vero omnis est mollitia repellendus aperiam ab a corrupti harum, odio quae neque maiores?</dd>
+          </ol>
+        </div>
+        <div class="col">
+          <h4 class="text-center">Makna Bentuk Lambang Museum Blambangan</h4>
+          <ol>
+            <li>Makna Lambang Museum Blambangan</li>
+            <dd> Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit aliquam porro qui ratione at, fuga velit vero omnis est mollitia repellendus aperiam ab a corrupti harum, odio quae neque maiores?</dd>
+            <li>Makna Lambang Museum Blambangan</li>
+            <dd> Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit aliquam porro qui ratione at, fuga velit vero omnis est mollitia repellendus aperiam ab a corrupti harum, odio quae neque maiores?</dd>
+            <li>Makna Lambang Museum Blambangan</li>
+            <dd> Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit aliquam porro qui ratione at, fuga velit vero omnis est mollitia repellendus aperiam ab a corrupti harum, odio quae neque maiores?</dd>
+            <li>Makna Lambang Museum Blambangan</li>
+            <dd> Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit aliquam porro qui ratione at, fuga velit vero omnis est mollitia repellendus aperiam ab a corrupti harum, odio quae neque maiores?</dd>
+          </ol>
+        </div>
+      </div>
+      </div>
+
+
+</section>
+
+<section id="contactus">
+  <div id="contact" class="justify-content-center mt-5 mb-5">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col">
+          <h3 class="text-center">Masukan</h3>
+          <div class="row mb-3 mt-5">
+            <div class="col-md-6">
+              <label for="nama" class="form-label">Nama :</label>
+              <input type="text" class="form-control" id="nama" placeholder="Nama">
+            </div>
+
+            <div class="col-md-6">
+              <label for="email" class="form-label">Email :</label>
+              <input type="email" class="form-control" id="email" placeholder="email">
+            </div>
+          </div>
+          <div class="mb-3">
+            <label for="pesan" class="form-label">Pesan :</label>
+            <textarea class="form-control" id="pesan" rows="3" placeholder="Masukkan pesan mu disini..."></textarea>
+          </div>
+          <div class="d-flex">
+            <button type="button" class="btn p-3 "><i class="fas fa-envelope me-3"></i>Kirim</button>
+          </div>
+        </div>
+        <div class="col text-center">
+          <h3> Lokasi Museum</h3>
+          <iframe class="text-center mt-5" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3948.819281900675!2d114.36570421486594!3d-8.220921594083931!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd14532c96b124f%3A0x879dd27dc1ad393e!2sDinas%20Kebudayaan%20dan%20Pariwisata%20Kabupaten%20Banyuwangi!5e0!3m2!1sid!2sid!4v1629876759330!5m2!1sid!2sid" width="625" height="285" style="border:5px solid #d1d0d6" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
 @endsection
--- resources/views/Menu_Utama/Contact.blade.php
+++ resources/views/Menu_Utama/Contact.blade.php
@@ -6,7 +6,7 @@
     <div class="container">
       <div class="row justify-content-center">
         <div class="col">
-          <h3 class="text-center">Masukan</h3>
+          <h3 class="text-center mt-5 mb-5">Masukan</h3>
           <div class="row mb-3 mt-5">
             <div class="col-md-6">
               <label for="nama" class="form-label">Nama :</label>
@@ -26,7 +26,7 @@
             <button type="button" class="btn p-3 "><i class="fas fa-envelope me-3"></i>Kirim</button>
           </div>
         </div>
-        <div class="col text-center">
+        <div class="col text-center mt-5 mb-5">
           <h3> Lokasi Museum</h3>
           <iframe class="text-center mt-5" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3948.819281900675!2d114.36570421486594!3d-8.220921594083931!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd14532c96b124f%3A0x879dd27dc1ad393e!2sDinas%20Kebudayaan%20dan%20Pariwisata%20Kabupaten%20Banyuwangi!5e0!3m2!1sid!2sid!4v1629876759330!5m2!1sid!2sid" width="625" height="285" style="border:5px solid #d1d0d6" allowfullscreen="" loading="lazy"></iframe>
         </div>
